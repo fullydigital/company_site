006dca3bf28fe613a69e9ea53866f90e8f4c5ac5
Michael <m_reinert@outlook.com>
2024-01-10 22:40:03 +0100

    added a lot of things

---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+import Alpine from 'alpinejs';
+import collapse from '@alpinejs/collapse';
+
+Alpine.plugin(collapse);
+
+Alpine.start();
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./main.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        wesolveGray: '#bbbbc1',
+        wesolveGreen: '#314f3e'
+      }
+    },
+  },
+  plugins: [],
+}
+
